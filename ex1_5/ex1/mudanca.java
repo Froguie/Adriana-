4291aad3515cb229b6e3c3d3f6137942045aeaf9
Matheus Ambrósio <matheushan06@gmail.com>
2026-04-13 17:38:51 -0300

    Add files via upload

---
+++ ex1_5/ex1/mudanca.java
@@ -0,0 +1,8 @@
+package ex_Adriana1.ex1;
+
+public class mudanca {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // prints Hello, World!
+    }
+
+}
---
+++ ex1_5/ex3/quebra.java
@@ -0,0 +1,8 @@
+package ex_Adriana1.ex3;
+
+public class quebra {
+    public static void main(String[] args) {
+        System.out.println("My name \nis Scracth");
+    }
+    
+}
