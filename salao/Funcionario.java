c4e8fab8a526941a4c46998482312afd5a191765
Matheus Ambrósio <matheushan06@gmail.com>
2026-04-15 15:33:16 -0300

    Add birthday and promotion methods to Funcionario

--- salao/Funcionario.java
+++ salao/Funcionario.java
@@ -33,4 +33,12 @@ public class Funcionario {
     public void inicio_turno() {
 			System.out.println(nome + " iniciou o turno.");
 		}
+	public void fazerAniversario() {
+    this.idade++;
+    System.out.println(nome + " fez aniversário! Nova idade: " + idade);
+	}
+	public void promover(String novoCargo) {
+    System.out.println(nome + " foi promovido de " + funcao + " para " + novoCargo);
+    this.funcao = novoCargo;
+}
 }
