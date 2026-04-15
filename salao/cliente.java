cb3b3168146dad88811128eb5c41b0924a52ce85
Matheus Ambrósio <matheushan06@gmail.com>
2026-04-15 15:22:16 -0300

    Create cliente.java

---
+++ salao/cliente.java
@@ -0,0 +1,33 @@
+package salao;
+
+
+public class Cliente {
+    private String nome;
+    private int idade;
+    private String telefone;
+
+
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public void mostrarinfo() {
+	    System.out.println("Nome: "+nome);
+	    System.out.println("\nTelefone: "+telefone);
+	    System.out.println("\nIdade: "+idade);
+	}
+}
