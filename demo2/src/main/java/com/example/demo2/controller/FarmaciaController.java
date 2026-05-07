fdb273a8b240e08f8b0bacaf8ffb5ae4c97bd324
Matheus Ambrósio <matheushan06@gmail.com>
2026-05-07 19:05:48 -0300

    Criar FarmaciaController.java

---
+++ demo2/src/main/java/com/example/demo2/controller/FarmaciaController.java
@@ -0,0 +1,54 @@
+package com.example.demo2.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FarmaciaController {
+
+    public void abrirCliente(ActionEvent event) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource(
+                                "/com/example/demo2/cliente.fxml"
+                        )
+                );
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Cliente");
+        stage.show();
+    }
+
+    public void abrirFuncionario(ActionEvent event) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource(
+                                "/com/example/demo2/funcionario.fxml"
+                        )
+                );
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Funcionário");
+        stage.show();
+    }
+
+    public void abrirRemedio(ActionEvent event) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource(
+                                "/com/example/demo2/remedio.fxml"
+                        )
+                );
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Remédio");
+        stage.show();
+    }
+}
