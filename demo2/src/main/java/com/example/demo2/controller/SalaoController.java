88acabe0a30238ded10a037a8441f21c540047b1
Matheus Ambrósio <matheushan06@gmail.com>
2026-05-07 19:07:54 -0300

    Criar SalaoController.java

---
+++ demo2/src/main/java/com/example/demo2/controller/SalaoController.java
@@ -0,0 +1,54 @@
+package com.example.demo2.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SalaoController {
+
+    public void abrirCliente(ActionEvent event) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource(
+                                "/com/example/demo2/cliente.fxml"
+                        )
+                );
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Cliente");
+        stage.show();
+    }
+
+    public void abrirFuncionario(ActionEvent event) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource(
+                                "/com/example/demo2/funcionario.fxml"
+                        )
+                );
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Funcionário");
+        stage.show();
+    }
+
+    public void abrirCorte(ActionEvent event) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource(
+                                "/com/example/demo2/corte.fxml"
+                        )
+                );
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Corte");
+        stage.show();
+    }
+}
