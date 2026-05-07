bfc74984c951803378184a4a4dbb8bf108c38ded
Matheus Ambrósio <matheushan06@gmail.com>
2026-05-07 19:09:16 -0300

    Atualizar o SalaoController.java

--- demo2/src/main/java/com/example/demo2/controller/SalaoController.java
+++ demo2/src/main/java/com/example/demo2/controller/SalaoController.java
@@ -12,7 +12,7 @@ public class SalaoController {
         FXMLLoader loader =
                 new FXMLLoader(
                         getClass().getResource(
-                                "/com/example/demo2/cliente.fxml"
+                                "/com/example/demo2/clienteSalao.fxml"
                         )
                 );
 
@@ -27,7 +27,7 @@ public class SalaoController {
         FXMLLoader loader =
                 new FXMLLoader(
                         getClass().getResource(
-                                "/com/example/demo2/funcionario.fxml"
+                                "/com/example/demo2/funcionarioSalao.fxml"
                         )
                 );
 
