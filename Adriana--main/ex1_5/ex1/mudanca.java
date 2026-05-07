3fdae2f543b7a5ac5e2e9e0c7c77eca2a57771f2
Matheus Ambrósio <matheushan06@gmail.com>
2026-05-07 18:03:44 -0300

    Delete Adriana--main directory

--- Adriana--main/ex1_5/ex1/mudanca.java
+++
@@ -1,8 +0,0 @@
-package ex_Adriana1.ex1;
-
-public class mudanca {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); // prints Hello, World!
-    }
-
-}
--- Adriana--main/ex1_5/ex3/quebra.java
+++
@@ -1,8 +0,0 @@
-package ex_Adriana1.ex3;
-
-public class quebra {
-    public static void main(String[] args) {
-        System.out.println("My name \nis Scracth");
-    }
-    
-}
